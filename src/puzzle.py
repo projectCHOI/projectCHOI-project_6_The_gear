a83e0c70b6d0405d126bf0c2bbb6b1f5c1f944c0
YoonSukChoi <yoonsuk.choi93@gmail.com>
2026-06-15 09:48:24 +0900

    PlayerController

--- src/puzzle.py
+++ src/puzzle.py
@@ -1,3 +1,10 @@
 # 퍼즐 스테이지 구성, 정답 판정, 도르래 기믹 담당
 import pygame
 import math
+
+class PlayerController:
+    def __init__(self):
+        """플레이어의 마우스 입력 및 톱니바퀴 드래그 조작을 관리하는 클래스"""
+        self.selected_gear = None  # 현재 마우스로 붙잡고 있는 톱니바퀴 객체
+        self.is_dragging = False   # 드래그 중인지 여부
+        self.last_mouse_angle = 0.0 # 직전 프레임의 마우스 각도
