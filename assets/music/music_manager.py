840b488cb42bc1bb97cdc0c45444da86072a2ded
최윤석 <144885127+projectCHOI@users.noreply.github.com>
2026-06-27 13:48:16 +0900

    MusicManager
    class MusicManager:

--- assets/music/music_manager.py
+++ assets/music/music_manager.py
@@ -1,3 +1,10 @@
 import pygame
 import os
 import random
+
+class MusicManager:
+    def __init__(self):
+        self.base_path = os.path.dirname(__file__)
+        self.effects = {}
+        self.install_sounds = []  # 5개의 설치 효과음을 담을 리스트
+        self._load_assets()
