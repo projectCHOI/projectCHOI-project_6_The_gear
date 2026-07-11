f8147c31d88292ab0d70c3174800b9b45923f268
최윤석 <144885127+projectCHOI@users.noreply.github.com>
2026-07-11 13:05:12 +0900

    load_level
    def load_level(self, level_num):

--- assets/stage/puzzle.py
+++ assets/stage/puzzle.py
@@ -10,3 +10,11 @@ class PuzzleManager:
         self.clear_condition = None
         
         self.load_level(self.current_level)
+
+def load_level(self, level_num):
+        self.gears.clear()
+        self.is_cleared = False
+        self.current_level = level_num
+
+        # 문자열 포맷팅을 통해 stage01, stage02 등의 파일을 가변적으로 매핑
+        stage_module_name = f"stage{level_num:02d}_manager"
