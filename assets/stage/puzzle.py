fed5c5e3d857b73439683ce83dc12c180a6aeb01
최윤석 <144885127+projectCHOI@users.noreply.github.com>
2026-07-11 13:04:41 +0900

    PuzzleManager
    def __init__(self):

--- assets/stage/puzzle.py
+++ assets/stage/puzzle.py
@@ -1,3 +1,12 @@
 import pygame
 import sys
 import os
+
+class PuzzleManager:
+    def __init__(self):
+        self.gears = []          
+        self.current_level = 1   
+        self.is_cleared = False   
+        self.clear_condition = None
+        
+        self.load_level(self.current_level)
